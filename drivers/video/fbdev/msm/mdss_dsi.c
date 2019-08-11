580849289165fab27b68a85353bc74218912f706
Nikish21 <dasnikish2109@gmail.com>
2019-08-11 14:31:50 +0530

    Fix some build errors

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -3130,9 +3130,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 					rc);
 		}
 		break;
-	case MDSS_EVENT_UPDATE_LIVEDISPLAY:
-		rc = mdss_livedisplay_update(ctrl_pdata, (int)(unsigned long) arg);
-		break;
+	
 	default:
 		pr_debug("%s: unhandled event=%d\n", __func__, event);
 		break;
